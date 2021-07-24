df9c89f5518859930bb63ca4e6b076bc84175974
abfedena <abfedena@users.noreply.github.com>
2021-07-24 11:32:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Beansys testing demo1!'));
+app.get('/', (req, res) => res.send('Hello World Beansys testing demo1 wewe sdwsdwd!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
